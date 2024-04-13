bb3cda987d02325889703410657ac63cd958c1bb
dpazbar29 <145806563+dpazbar29@users.noreply.github.com>
2024-04-13 11:54:04 +0200

    Instalación de Ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,22 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 plugins {
     kotlin("jvm") version "1.9.21"
+    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
+}
+
+ktlint {
+    verbose.set(true)
+    outputToConsole.set(true)
+    coloredOutput.set(true)
+    reporters {
+        reporter(ReporterType.CHECKSTYLE)
+        reporter(ReporterType.JSON)
+        reporter(ReporterType.HTML)
+    }
+    filter {
+        exclude("**/style-violations.kt")
+    }
 }
 
 group = "org.example"
